be53a4e8d8074e009b0391a88fc8608ad3e0a116
Dryss <drysscark@gmail.com>
2024-10-30 08:17:34 +0100

    add new  fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,69 +2,115 @@
 
 namespace App\DataFixtures;
 
-use App\Entity\Category;
-use App\Entity\Movie;
+use Faker\Factory;
 use App\Entity\User;
-use App\Entity\WatchHistory;
+use Faker\Generator;
+use App\Entity\Movie;
+use App\Entity\Serie;
+use App\Entity\Category;
+use App\Entity\Language;
 use App\Enum\UserAccountStatusEnum;
-use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class AppFixtures extends Fixture
 {
+    private Generator $faker;
+
+    public function __construct()
+    {
+        $this->faker = Factory::create();
+    }
+
     public function load(ObjectManager $manager): void
     {
-        for ($i = 0; $i < 50; $i++) {
-            $film1 = new Movie();
-            $film1->setTitle('Film ' . $i);
-            $film1->setCoverImage('image' . $i . '.png');
-            $film1->setShortDescription('description courte : ' . $i);
-            $film1->setLongDescription('description longue : ' . $i);
-            $film1->setReleaseDate(new \DateTime());
-            $manager->persist($film1);
-        }
-
-        $film2 = new Movie();
-        $film2->setTitle('Avatar');
-        $film2->setCoverImage('avatar.png');
-        $film2->setShortDescription('ça parle de mec en bleu écolo');
-        $film2->setLongDescription('description longue de avatar');
-        $film2->setReleaseDate(new \DateTime('+1 day'));
-        $manager->persist($film2);
-
-        $user = new User();
-        $user->setUsername('baptiste');
-        $user->setEmail('bvasseur77@example.com');
-        $user->setPassword('motdepasse');
-        $user->setAccountStatus(UserAccountStatusEnum::ACTIVE);
-        $manager->persist($user);
-
-        $history1 = new WatchHistory();
-        $history1->setMedia($film1);
-        $history1->setWatcher($user);
-        $history1->setLastWatchedAt(new \DateTimeImmutable());
-        $history1->setNumberOfViews(5);
-        $manager->persist($history1);
-
-        $history2 = new WatchHistory();
-        $history2->setMedia($film2);
-        $history2->setWatcher($user);
-        $history2->setLastWatchedAt(new \DateTimeImmutable());
-        $history2->setNumberOfViews(5);
-        $manager->persist($history2);
-
-        $tableau = [
-            ['Action', 'action'],
-            ['Aventure', 'aventure'],
-        ];
-
-        foreach ($tableau as $element) {
-            $category = new Category();
-            $category->setLabel($element[1]);
-            $category->setNom($element[0]);
-            $manager->persist($category);
-        }
+        $users = $this->generateUsers($manager);
+        $medias = $this->generateMedias($manager);
+        $categories = $this->generateCategories($manager, $medias);
+        $languages = $this->generateLanguages($manager, $medias);
 
         $manager->flush();
+
+    }
+
+    private function generateUsers(ObjectManager $manager): array
+    {
+        $users = [];
+        for ($i = 0; $i < 50; $i++) {
+            $user = new User();
+            $user->setUsername($this->faker->userName);
+            $user->setEmail($this->faker->email);
+            $user->setPassword($this->faker->password);
+            $user->setAccountStatus(UserAccountStatusEnum::ACTIVE);
+            $users[] = $user;
+            $manager->persist($user);
+        }
+
+        return $users;
+    }
+
+    protected function generateLanguages(ObjectManager $manager, array $medias): array
+    {
+        $languages = [];
+        $languageData = [
+            ['fr', 'Français'], ['en', 'Anglais'], ['es', 'Espagnol'], ['de', 'Allemand'],
+            ['it', 'Italien'], ['pt', 'Portugais'], ['ru', 'Russe'], ['zh', 'Chinois'],
+            ['ja', 'Japonais'], ['ar', 'Arabe'], ['hi', 'Hindi'], ['bn', 'Bengali'],
+            ['ko', 'Coréen'], ['tr', 'Turc'], ['nl', 'Néerlandais'], ['pl', 'Polonais']
+        ];
+
+        foreach ($languageData as [$code, $name]) {
+            $entity = new Language();
+            $entity->setCode($code);
+            $entity->setNom($name);
+            $languages[] = $entity;
+            $manager->persist($entity);
+
+            foreach ($this->faker->randomElements($medias, rand(0, 20)) as $media) {
+                $media->addLanguage($entity);
+            }
+        }
+
+        return $languages;
+    }
+
+    protected function generateCategories(ObjectManager $manager, array $medias): array
+    {
+        $categories = [];
+        $categoryNames = ['Action', 'Aventure', 'Comédie', 'Drame', 'Fantastique', 'Horreur', 'Policier', 'Science-fiction', 'Thriller'];
+
+        foreach ($categoryNames as $name) {
+            $category = new Category();
+            $category->setLabel($name);
+            $category->setNom(strtolower($this->faker->unique()->slug(1)));
+            $categories[] = $category;
+            $manager->persist($category);
+
+
+            foreach ($this->faker->randomElements($medias, rand(0, 20)) as $media) {
+                $media->addCategory($category);
+            }
+        }
+
+        return $categories;
+    }
+
+    protected function generateMedias(ObjectManager $manager): array
+    {
+        $medias = [];
+        for ($j = 0; $j < 50; $j++) {
+            $type = $this->faker->randomElement([Movie::class, Serie::class]);
+            $media = new $type();
+            $media->setTitle($this->faker->sentence(3));
+            $media->setShortDescription($this->faker->text(50));
+            $media->setLongDescription($this->faker->text(200));
+            $media->setCoverImage("cover_image_{$j}.png");
+            $media->setReleaseDate($this->faker->dateTimeThisDecade());
+            $media->setCasting($this->faker->words(rand(2, 5)));
+            $media->setStaff($this->faker->words(rand(2, 5)));
+            $medias[] = $media;
+            $manager->persist($media);
+        }
+        return $medias;
     }
 }
